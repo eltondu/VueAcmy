fcc3a268b9b16e8700481c8a79cd3cd1c706d2c7
eltondu <elton.du@gmail.com>
2022-05-09 15:02:34 -0300

    Updt Sync

--- src/views/SyncDatagrid.vue
+++ src/views/SyncDatagrid.vue
@@ -1,39 +1,54 @@
 <template>
 <div class="col-lg-12 control-section">
-    <div>
+    <div class="m-4">
         <ejs-grid :dataSource="data" :allowPaging='true' :pageSettings='pageSettings' :editSettings='editSettings' :toolbar='toolbar'>
             <e-columns>
-            <e-column field='OrderID' headerText='Order ID' width='120' textAlign='Right' :isPrimaryKey='true' :validationRules='orderidrules'></e-column>
-            <e-column field='CustomerID' headerText='Customer ID' width='120' :validationRules='customeridrules'></e-column>
-            <e-column field='Freight' headerText='Freight' width='120' format='C2' textAlign='Right' editType='numericedit' :validationRules='freightrules'></e-column>
-            <e-column field='OrderDate' headerText='Order Date' width='130' editType='datepickeredit' format='yMd' textAlign='Right' type='date'></e-column>
-            <e-column field='ShipCountry' headerText='Ship Country' width='150' editType='dropdownedit' :edit='editparams'></e-column>
-            <e-column field='Verified' headerText='Verified' width='100' editType='booleanedit' displayAsCheckBox='true' type='boolean' textAlign='Center'></e-column>
+            <e-column field='lastName' headerText='Last Name' width='120'></e-column>
+            <e-column field='firstName' headerText='First Name' width='120'></e-column>
+            <e-column field='id' headerText='Id' width='120' textAlign='Right' editType='numericedit' :isPrimaryKey='true'></e-column>
+            <e-column field='idBook' headerText='Book Id' width='130' editType='numericedit' textAlign='Right'></e-column>                        
         </e-columns> 
         </ejs-grid>
     </div>
-
 </div>
 </template>
 <script lang="ts">
 import Vue from "vue";
 import { GridPlugin, Edit, Toolbar, Page } from "@syncfusion/ej2-vue-grids";
-import  orderDetails  from "../data/sync.json";
+
+import axios from "axios"
 
 Vue.use(GridPlugin);
 
+const orderDetails = []
+
 export default Vue.extend({
   data: () => {
-    return {
-      data: orderDetails.slice(0),
+    return {        
+      data: [],
       editSettings: { allowEditing: true, allowAdding: true, allowDeleting: true, mode: 'Dialog' },
       toolbar: ['Add', 'Edit', 'Delete'],
-      orderidrules: { required: true, number: true },
-      customeridrules: { required: true },
-      freightrules:  { required: true },
       editparams: { params: { popupHeight: '300px' }},
       pageSettings: { pageCount: 5}
     };
+  },
+    mounted() {
+    axios
+      .get("https://fakerestapi.azurewebsites.net/api/v1/Authors")
+      .then((res) => {
+          console.log(res)
+        var before = Date.now();
+        var elapsed = Date.now() - before;
+        setTimeout(() => {
+          this.data = res.data;
+        //   this.tableRows = res.data;
+        //   this.isBusy = !this.isBusy;
+        //   this.totalRows = this.orderDetails.length;
+        }, elapsed);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   },
   provide: {
       grid: [Edit, Toolbar, Page]
